5d7e9da8fd563fd6e7eb7658ec7ac9a8b308b38b
taahahussainkhan <taaha128@gmail.com>
2024-08-05 05:50:51 +0000

    [LeetCode Sync] Runtime - 75 ms (84.29%), Memory - 16.8 MB (55.96%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.py
+++ my-folder/0004-median-of-two-sorted-arrays/solution.py
@@ -1,12 +1,12 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = nums1 + nums2
-        merged.sort()
-        n = len(merged)
-        
-        if n % 2 == 0:
-            mid = merged[n//2]
-            mid1 = merged[n//2 - 1]
-            return (mid + mid1) / 2.0
-        else:
-            return float(merged[n //2])
+        def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+                merged = nums1 + nums2
+                merged.sort()
+                n = len(merged)
+                                        
+                if n % 2 == 0:
+                    mid = merged[n//2]
+                    mid1 = merged[n//2 - 1]
+                    return (mid + mid1) / 2.0
+                else:
+                    return float(merged[n //2])
